d048ac5ad5de9203528eb18f72d3cded13fa9cd3
minh phan <trongminh20@gmail.com>
2019-01-29 14:28:22 -0800

    apply the try-catch block for whole code

--- term2/Assign2/src/arrayList.java
+++ term2/Assign2/src/arrayList.java
@@ -33,27 +33,3 @@ public class arrayList{
         }
     }
 }
-class Student {
-   private String student;
-
-    Student(String student){
-        this.student = student;
-    }
-
-    //getters
-    public String getName(){
-        String[] a = student.split(" ");
-        return a[0];
-    }
-    public int getAge(){
-        String[] a = student.split(" ");
-        return Integer.parseInt(a[1]);
-    }
-    public int getGrade(){
-        String[] a = student.split(" ");
-        return Integer.parseInt(a[2]);
-    }
-    public String toString(){
-        return student;
-    }
-}
--- term2/assignment3/src/studentManager.java
+++ term2/assignment3/src/studentManager.java
@@ -1,60 +1,66 @@
+/*
+Name: Minh Phan
+ID: 300269120
+* */
 import java.util.*;
 
 public class studentManager{
     public static void main(String[] args) {
          boolean isValid = false;
-         Scanner stdin = new Scanner(System.in);
+         int grade;
+
          String name;
          ArrayList<Student> st = new ArrayList<Student>();
-         int grade;
+
+         Scanner stdin = new Scanner(System.in);
+
          while(!isValid){
-             switch(makechoice()){
-                 case 1:
-                     try{
+             try{
+                 switch(makeChoices()){
+                     case 1:
                          System.out.println("Please enter the students name: ");
                          name = stdin.next();
                          System.out.println("Please enter a grade for "+name+":");
                          grade = stdin.nextInt();
                          Student a = new Student(name, grade);
                          st.add(a);
-                     }catch (InputMismatchException e){
-                         System.out.println("You entered wrong input!!");
-                     }catch (IndexOutOfBoundsException ex){
-                         System.out.println("You entered wrong input!!");
-                     }
-                     finally {
-                         stdin.nextLine();
                          break;
-                     }
-
-                 case 2:
-                     System.out.println("What student would you like to enter a grade for?");
-                     for(int count = 0; count < st.size(); count++){
-                         System.out.println((count+1)+") "+st.get(count));
-                     }
-
-                     int inputAgain = stdin.nextInt();
-                     System.out.println("Please enter a grade for "+st.get(inputAgain-1).getName());
-                     grade = stdin.nextInt();
-                     st.get(inputAgain-1).setGrade(grade);
-                     break;
-
-                 case 3:
-                     System.out.println("Thank you, Goodbye!");
-                     isValid = true;
-             }//end switch
+                    case 2:
+                         System.out.println("What student would you like to enter a grade for?");
+                         for(int count = 0; count < st.size(); count++){
+                             System.out.println((count+1)+") "+st.get(count));
+                         }
+                         int inputAgain = stdin.nextInt();
+                         System.out.println("Please enter a grade for "+st.get(inputAgain-1).getName());
+                         grade = stdin.nextInt();
+                         st.get(inputAgain-1).setGrade(grade);
+                         break;
+                    case 3:
+                         isValid = true;
+                 }//end switch
+             }//end try block
+             catch (InputMismatchException e){
+                 stdin.nextLine();
+                System.out.println("You entered wrong input, please try again!!");
+             }catch (IndexOutOfBoundsException ex){
+                 stdin.nextLine();
+                System.out.println("You entered wrong input, please try again!!");
+             }
          }//end while
 
-         //System.out.println(st.toString());
+         System.out.println("Thank you, Goodbye!");
 
-    }//end main
+    }//end main method
 
-    public static int makechoice(){
+    public static int makeChoices(){
         Scanner stdin = new Scanner(System.in);
         int choice = 0;
         try {
-            String warm = "Main menu\n\n\nSelect one of the following options\n1. Add a student\n2. Edit student " +
-                    "grades\n3. Exit";
+            String warm = "\nMain menu\n\n\n" +
+                    "Select one of the following options\n" +
+                    "1. Add a student\n2. Edit student " +
+                    "grades\n" +
+                    "3. Exit";
             System.out.println(warm);
             choice = stdin.nextInt();
         }catch(InputMismatchException e){
